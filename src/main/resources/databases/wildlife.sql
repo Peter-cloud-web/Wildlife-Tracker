b1d3d3837c606acb2a1665be5cd416cc42dde91a
Peter-cloud-web <petergichia164@gmail.com>
2020-02-09 13:51:36 +0300

    create endangered animal table with the necessary attributes

--- src/main/resources/databases/wildlife.sql
+++ src/main/resources/databases/wildlife.sql
@@ -14,3 +14,10 @@ CREATE TABLE sightings(
  created at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE endangered_animals(
+  id SERIAL PRIMARY KEY,
+  animal_id int,
+  animal_name VARCHAR ,
+  animal_age VARCHAR,
+  animal_health VARCHAR
+);
