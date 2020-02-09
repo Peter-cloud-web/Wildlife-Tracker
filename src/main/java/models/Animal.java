b3d498d4aa0c5acbe3c1a890816e28db21abb2cb
Peter-cloud-web <petergichia164@gmail.com>
2020-02-09 09:01:50 +0300

    Use interface to store methods to be implemented

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,53 @@
 package models;
 
-public class Animal {
+import Interfaces.AnimalInterface;
+
+public class Animal implements AnimalInterface {
+
+    private String animalName;
+    private String animalHealth;
+    private int animalAge;
+    private int id;
+
+
+    public Animal(String animalName, String animalHealth, int animalAge, int id) {
+        this.animalName = animalName;
+        this.animalHealth = animalHealth;
+        this.animalAge = animalAge;
+        this.id = id;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalHealth() {
+        return animalHealth;
+    }
+
+    public void setAnimalHealth(String animalHealth) {
+        this.animalHealth = animalHealth;
+    }
+
+    public int getAnimalAge() {
+        return animalAge;
+    }
+
+    public void setAnimalAge(int animalAge) {
+        this.animalAge = animalAge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
+
+
