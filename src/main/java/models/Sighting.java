adbbc41c5ec1a447f47023466c57f544337f6641
Peter-cloud-web <petergichia164@gmail.com>
2020-02-09 09:07:55 +0300

    import interfaces for method implementation in child classes

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,4 +1,50 @@
 package models;
 
-public class Sighting {
+import Interfaces.SightingInterface;
+
+public class Sighting implements SightingInterface {
+    private String rangerName;
+    private String animalLocation;;
+    private int animal_id;
+    private int id;
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public void setRangerName(String rangerName) {
+        this.rangerName = rangerName;
+    }
+
+    public String getAnimalLocation() {
+        return animalLocation;
+    }
+
+    public void setAnimalLocation(String animalLocation) {
+        this.animalLocation = animalLocation;
+    }
+
+    public int getAnimal_id() {
+        return animal_id;
+    }
+
+    public void setAnimal_id(int animal_id) {
+        this.animal_id = animal_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Sighting(String rangerName, String animalLocation, int animal_id, int id) {
+        this.rangerName = rangerName;
+        this.animalLocation = animalLocation;
+        this.animal_id = animal_id;
+        this.id = id;
+    }
+
 }
